8e4f9bd5d1eb9dc83222c972372ac6b7fc208e5e
Joe Fazzino <joe@fluid.work>
2018-03-07 15:14:47 +0000

    Commit Project Files

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HybridApp from './src/App'
+
+export default class NativeApp extends React.Component {
+  render() {
+    return <HybridApp />
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { View, Text, Image, Animated, StyleSheet, StatusBar } from 'react-native'
+import { duration } from 'moment';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.imageAnimation = new Animated.Value(0);
+    }
+    
+    componentDidMount() {
+        Animated.loop(
+          Animated.timing(this.imageAnimation, {
+              toValue: 1,
+              duration: 1005
+          })
+        ).start();
+
+        StatusBar.setBarStyle('light-content');
+    }
+
+    render() {
+        const rotationStyle = { transform: [{
+            rotate: this.imageAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '360deg']
+            })
+        }] };
+
+        return (
+            <View style={styles.app}>
+                <View style={styles.appHeader}>
+                    <Animated.Image 
+                        style={[styles.headerImage, rotationStyle]} 
+                        resizeMode={'contain'} 
+                        source={require('./assets/react-logo.png')} 
+                    />
+                    <Text style={styles.appTitle}>Welcome to React Native Web️</Text>
+                </View>
+                <Text style={styles.appIntro}>
+                    To get started, edit src/App.js and save to reload.
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    app: {
+        flex: 1
+    },
+    appHeader: {
+        flex: 1,
+        backgroundColor: '#222',
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerImage: {
+        width: 200,
+        height: 200,
+        flex: 3
+    },
+    appTitle: {
+        flex: 1,
+        fontSize: 16,
+        color: 'white'
+    },
+    appIntro: {
+        flex: 3,
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
