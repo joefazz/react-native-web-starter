4e366b950765afb71da8d9ebc011116143363f8b
Joe Fazzino <joe@fluid.work>
2018-04-09 11:18:18 +0100

    Update master

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import { Provider } from 'react-redux';
-import HybridApp from './src/App'
-import store from './src/redux/configureStore';
+import React from "react";
+import HybridApp from "./src/App";
 
 export default class NativeApp extends React.Component {
     render() {
-        return (
-            <Provider store={store}>
-                <HybridApp />
-            </Provider>
-        );
+        return <HybridApp />;
     }
 }
