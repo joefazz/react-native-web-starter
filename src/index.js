92087e4f34a8d2517b8fb4f3336bf15012c40ff2
Wisniewski, Luke <lukewis@users.noreply.github.com>
2018-04-11 05:32:45 -0400

    - Added typescript support

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Note: This file was not converted to typescript because of issues with module resolution in registerServiceWorker()
+// This level is fairly static anyways, so there isn't a huge benefit to converting it
 ReactDOM.render(
     <App />
     , document.getElementById('root'));
