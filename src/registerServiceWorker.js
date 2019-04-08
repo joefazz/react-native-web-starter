cd27198c38e251f6824beaf1d0928a7c66cc34a2
Christopher J. Brody <chris@brody.consulting>
2019-04-08 17:02:14 -0400

    [WIP] yarn lint task WITH A TEMPORARY TRICK
    NOTE that this has a trick to use eslint-config-standard-jsx
    to quickly get a very basic working set of eslint
    rules working with React that is up-to-date.
    
    TODO: we need a new eslint config package that is designed to
    gives us the basic set of working eslint rules
    without using this kind of a trick.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,7 @@
+/* eslint-env browser */
+/* eslint-env node */
+/* eslint no-console: off */
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
