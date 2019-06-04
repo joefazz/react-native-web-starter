a20b97ad75329757d18c6a9eb851b46da45ea24b
Aiham Hammami <aiham@users.noreply.github.com>
2019-06-04 11:27:21 -0400

    Strip component prop from Link component on web platform

--- src/Routing.web.js
+++ src/Routing.web.js
@@ -1,6 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const FixedLink = ({ component, children, ...props }) => <Link {...props}>{children}</Link>;
+
+export { FixedLink as Link };
+
 export {
     BrowserRouter as Router,
-    Link,
     Switch,
     Route
 } from 'react-router-dom';
